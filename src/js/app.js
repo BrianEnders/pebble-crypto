a38e8f1a8bf7281d22690b59a193eb8357167799
BrianEnders <ouellette.brian@outlook.com>
2017-12-13 14:59:34 -0800

    Commit Source

---
+++ src/js/app.js
@@ -0,0 +1,236 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+var Clay = require('clay');
+var sb = require('bitcoin');
+var Settings = require('settings');
+var clayConfig = require('config');
+var clay = new Clay(clayConfig, null, { autoHandleEvents: false });
+
+var Feature = require('platform/feature');
+var ajax = require('ajax');
+
+// Global UI 
+var splashCard = new UI.Card();
+var routesMenu = new UI.Menu();
+var fail_bg = '#b30000';
+
+var primary = '#111111';
+var secondary = '#DDDDDD';
+
+var prices = [];
+var wallets = [];
+
+// Gets this all going
+loadPrices();
+
+function displaySplashScreen(message, bg_color){
+  
+  // Text element to inform user
+  splashCard = new UI.Card({
+    status: {
+      separator: 'none',
+    },
+    title: message,
+    titleColor:'#FFFFFF',
+    textAlign:'center',
+    backgroundColor: Feature.color(bg_color, 'black')
+  });
+  
+  splashCard.show();
+}
+
+
+/**
+ * Builds a menu item with routes data
+ * @param data for menu
+ */
+function displayStuff(){
+  // Construct Menu to show to user
+	
+	var menuView = [];
+	
+	if(wallets.length > 0)
+	{
+		var priceAmount = (wallets[0].base=="BTC") ? prices[0].amount : prices[1].amount;
+		var fixAmount = priceAmount * sb.toBitcoin(wallets[0].balance);
+		menuView.push({
+			subtitle:sb.toBitcoin(wallets[0].balance)+((wallets[0].base=="BTC") ? " BTC" : " LTC"),
+			title:parseFloat(fixAmount).toFixed(2)
+		});
+	}
+	
+	if(wallets.length > 1)
+	{
+    var priceAmount = (wallets[1].base=="BTC") ? prices[0].amount : prices[1].amount;
+		var fixAmount = priceAmount * sb.toBitcoin(wallets[1].balance);
+		menuView.push({
+			subtitle:sb.toBitcoin(wallets[1].balance) +((wallets[1].base=="BTC") ? " BTC" : " LTC"),
+			title:parseFloat(fixAmount).toFixed(2)
+		});
+	}
+		
+	menuView.push({
+		title:prices[0].amount,
+		icon:'images/btcicon.png',
+		subtitle:prices[0].currency+" Coinbase"
+	});
+
+	menuView.push({
+		title:prices[1].amount,
+		icon:'images/ltcicon.png',
+		subtitle:prices[1].currency+" Coinbase"
+	});
+	
+
+	
+  // Add data & style to menu
+  routesMenu = new UI.Menu({
+    status: {
+      separator: 'none',
+    },
+    backgroundColor: Feature.color(primary, 'black'),
+    highlightBackgroundColor: Feature.color(secondary, 'white'),
+    textColor: Feature.color(secondary, 'white'),
+    highlightTextColor: Feature.color('#222222', 'black'),
+    sections: [{
+      items: menuView
+    }]
+  });
+
+  // Show the Menu, hide the splash
+  routesMenu.show();
+  splashCard.hide(); 
+}
+
+/**
+ * Ajax call for route data.
+ */
+function loadPrices(){
+  displaySplashScreen('Downloading Data...', primary);
+	var currency = Settings.option("KEY_EXCHANGE_CUR") == null ? "USD" : Settings.option("KEY_EXCHANGE_CUR");
+  var getBTCPrice = "https://api.coinbase.com/v2/prices/BTC-"+currency+"/spot";
+	var getLTCPrice = "https://api.coinbase.com/v2/prices/LTC-"+currency+"/spot";
+  // Make the request for route data
+	
+  ajax(
+    {
+      url: getBTCPrice,
+      type: 'json'
+    },
+    function(data) {
+      // Success
+			
+			prices.push({
+				base:data.data.base,
+				amount:data.data.amount,
+				currency:data.data.currency
+			});
+
+			ajax(
+				{
+					url: getLTCPrice,
+					type: 'json'
+				},
+				function(data) {
+					// Success
+					prices.push({
+						base:data.data.base,
+						amount:data.data.amount,
+						currency:data.data.currency
+					});
+					loadBTCWallet();
+				},
+				function(error) {
+					// Failure!
+					displaySplashScreen('Failed to load LTC.', fail_bg);
+				}
+			);
+    },
+    function(error) {
+      // Failure!
+      displaySplashScreen('Failed to load BTC.', fail_bg);
+    }
+  );
+}
+
+function loadBTCWallet(){
+	var balanceBTCUrl = "https://api.blockcypher.com/v1/btc/main/addrs/"+Settings.option("KEY_BTC_ADDRESS_1")+"/balance";
+	if(Settings.option("KEY_BTC_ADDRESS_1") != null && Settings.option("KEY_BTC_ADDRESS_1") != "")
+	{
+		ajax(
+			{
+				url: balanceBTCUrl,
+				type: 'json'
+			},
+			function(data) {
+				// Success
+
+				wallets.push({
+					balance:data.final_balance,
+					address:data.address,
+					base:"BTC"
+				});
+
+				loadLTCWallet();
+			},
+			function(error) {
+				// Failure!
+				displaySplashScreen('Failed to load BTC Wallet.', fail_bg);
+			}
+		);
+	}else{
+		loadLTCWallet();
+	}
+}
+
+function loadLTCWallet(){
+	var balanceLTCUrl = "https://api.blockcypher.com/v1/ltc/main/addrs/"+Settings.option("KEY_LTC_ADDRESS_1")+"/balance";
+	
+	if(Settings.option("KEY_LTC_ADDRESS_1") != null && Settings.option("KEY_LTC_ADDRESS_1") != "")
+	{
+		ajax(
+			{
+				url: balanceLTCUrl,
+				type: 'json'
+			},
+			function(data) {
+				// Success
+				wallets.push({
+					balance:data.final_balance,
+					address:data.address,
+					base:"LTC"
+				});
+
+				displayStuff();
+			},
+			function(error) {
+				// Failure!
+				displaySplashScreen('Failed to load LTC Wallet.', fail_bg);
+			}
+		);
+	}else{
+		displayStuff();
+	}
+}
+  
+Pebble.addEventListener('showConfiguration', function(e) {
+  Pebble.openURL(clay.generateUrl());
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  if (e && !e.response) {
+    return;
+  }
+  var dict = clay.getSettings(e.response);
+  Settings.option(dict);
+	prices = [];
+  wallets = [];
+	routesMenu.hide();
+  splashCard.hide();
+	loadPrices();
+});
---
+++ src/js/config.js
@@ -0,0 +1,784 @@
+module.exports = [
+  {
+    "type": "heading",
+    "defaultValue": "Crypto Checker"
+  },
+  {
+    "type": "text",
+    "defaultValue": "Configure your Wallet Address information here"
+  },
+	{
+    "type": "section",
+    "items": 
+		[
+      {
+        "type": "heading",
+        "defaultValue": "Exchange Value"
+      },
+      {
+				"type": "select",
+				"appKey": "KEY_EXCHANGE_CUR",
+				"defaultValue": "USD",
+				"options": [
+					{
+						"value": "USD",
+						"label": "United States Dollar"
+					},
+					{
+						"value": "AED",
+						"label": "United Arab Emirates Dirham"
+					},
+					{
+						"value": "AFN",
+						"label": "Afghan Afghani"
+					},
+					{
+						"value": "ALL",
+						"label": "Albanian Lek"
+					},
+					{
+						"value": "AMD",
+						"label": "Armenian Dram"
+					},
+					{
+						"value": "ANG",
+						"label": "Netherlands Antillean Gulden"
+					},
+					{
+						"value": "AOA",
+						"label": "Angolan Kwanza"
+					},
+					{
+						"value": "ARS",
+						"label": "Argentine Peso"
+					},
+					{
+						"value": "AUD",
+						"label": "Australian Dollar"
+					},
+					{
+						"value": "AWG",
+						"label": "Aruban Florin"
+					},
+					{
+						"value": "AZN",
+						"label": "Azerbaijani Manat"
+					},
+					{
+						"value": "BAM",
+						"label": "Bosnia and Herzegovina Convertible Mark"
+					},
+					{
+						"value": "BBD",
+						"label": "Barbadian Dollar"
+					},
+					{
+						"value": "BDT",
+						"label": "Bangladeshi Taka"
+					},
+					{
+						"value": "BGN",
+						"label": "Bulgarian Lev"
+					},
+					{
+						"value": "BHD",
+						"label": "Bahraini Dinar"
+					},
+					{
+						"value": "BIF",
+						"label": "Burundian Franc"
+					},
+					{
+						"value": "BMD",
+						"label": "Bermudian Dollar"
+					},
+					{
+						"value": "BND",
+						"label": "Brunei Dollar"
+					},
+					{
+						"value": "BOB",
+						"label": "Bolivian Boliviano"
+					},
+					{
+						"value": "BRL",
+						"label": "Brazilian Real"
+					},
+					{
+						"value": "BSD",
+						"label": "Bahamian Dollar"
+					},
+					{
+						"value": "BTC",
+						"label": "Bitcoin"
+					},
+					{
+						"value": "BTN",
+						"label": "Bhutanese Ngultrum"
+					},
+					{
+						"value": "BWP",
+						"label": "Botswana Pula"
+					},
+					{
+						"value": "BYN",
+						"label": "Belarusian Ruble"
+					},
+					{
+						"value": "BYR",
+						"label": "Belarusian Ruble"
+					},
+					{
+						"value": "BZD",
+						"label": "Belize Dollar"
+					},
+					{
+						"value": "CAD",
+						"label": "Canadian Dollar"
+					},
+					{
+						"value": "CDF",
+						"label": "Congolese Franc"
+					},
+					{
+						"value": "CHF",
+						"label": "Swiss Franc"
+					},
+					{
+						"value": "CLF",
+						"label": "Unvaluead de Fomento"
+					},
+					{
+						"value": "CLP",
+						"label": "Chilean Peso"
+					},
+					{
+						"value": "CNY",
+						"label": "Chinese Renminbi Yuan"
+					},
+					{
+						"value": "COP",
+						"label": "Colombian Peso"
+					},
+					{
+						"value": "CRC",
+						"label": "Costa Rican Col\u00f3n"
+					},
+					{
+						"value": "CUC",
+						"label": "Cuban Convertible Peso"
+					},
+					{
+						"value": "CVE",
+						"label": "Cape Verdean Escudo"
+					},
+					{
+						"value": "CZK",
+						"label": "Czech Koruna"
+					},
+					{
+						"value": "DJF",
+						"label": "Djiboutian Franc"
+					},
+					{
+						"value": "DKK",
+						"label": "Danish Krone"
+					},
+					{
+						"value": "DOP",
+						"label": "Dominican Peso"
+					},
+					{
+						"value": "DZD",
+						"label": "Algerian Dinar"
+					},
+					{
+						"value": "EEK",
+						"label": "Estonian Kroon"
+					},
+					{
+						"value": "EGP",
+						"label": "Egyptian Pound"
+					},
+					{
+						"value": "ERN",
+						"label": "Eritrean Nakfa"
+					},
+					{
+						"value": "ETB",
+						"label": "Ethiopian Birr"
+					},
+					{
+						"value": "EUR",
+						"label": "Euro"
+					},
+					{
+						"value": "FJD",
+						"label": "Fijian Dollar"
+					},
+					{
+						"value": "FKP",
+						"label": "Falkland Pound"
+					},
+					{
+						"value": "GBP",
+						"label": "British Pound"
+					},
+					{
+						"value": "GEL",
+						"label": "Georgian Lari"
+					},
+					{
+						"value": "GGP",
+						"label": "Guernsey Pound"
+					},
+					{
+						"value": "GHS",
+						"label": "Ghanaian Cedi"
+					},
+					{
+						"value": "GIP",
+						"label": "Gibraltar Pound"
+					},
+					{
+						"value": "GMD",
+						"label": "Gambian Dalasi"
+					},
+					{
+						"value": "GNF",
+						"label": "Guinean Franc"
+					},
+					{
+						"value": "GTQ",
+						"label": "Guatemalan Quetzal"
+					},
+					{
+						"value": "GYD",
+						"label": "Guyanese Dollar"
+					},
+					{
+						"value": "HKD",
+						"label": "Hong Kong Dollar"
+					},
+					{
+						"value": "HNL",
+						"label": "Honduran Lempira"
+					},
+					{
+						"value": "HRK",
+						"label": "Croatian Kuna"
+					},
+					{
+						"value": "HTG",
+						"label": "Haitian Gourde"
+					},
+					{
+						"value": "HUF",
+						"label": "Hungarian Forint"
+					},
+					{
+						"value": "valueR",
+						"label": "Indonesian Rupiah"
+					},
+					{
+						"value": "ILS",
+						"label": "Israeli New Sheqel"
+					},
+					{
+						"value": "IMP",
+						"label": "Isle of Man Pound"
+					},
+					{
+						"value": "INR",
+						"label": "Indian Rupee"
+					},
+					{
+						"value": "IQD",
+						"label": "Iraqi Dinar"
+					},
+					{
+						"value": "ISK",
+						"label": "Icelandic Kr\u00f3na"
+					},
+					{
+						"value": "JEP",
+						"label": "Jersey Pound"
+					},
+					{
+						"value": "JMD",
+						"label": "Jamaican Dollar"
+					},
+					{
+						"value": "JOD",
+						"label": "Jordanian Dinar"
+					},
+					{
+						"value": "JPY",
+						"label": "Japanese Yen"
+					},
+					{
+						"value": "KES",
+						"label": "Kenyan Shilling"
+					},
+					{
+						"value": "KGS",
+						"label": "Kyrgyzstani Som"
+					},
+					{
+						"value": "KHR",
+						"label": "Cambodian Riel"
+					},
+					{
+						"value": "KMF",
+						"label": "Comorian Franc"
+					},
+					{
+						"value": "KRW",
+						"label": "South Korean Won"
+					},
+					{
+						"value": "KWD",
+						"label": "Kuwaiti Dinar"
+					},
+					{
+						"value": "KYD",
+						"label": "Cayman Islands Dollar"
+					},
+					{
+						"value": "KZT",
+						"label": "Kazakhstani Tenge"
+					},
+					{
+						"value": "LAK",
+						"label": "Lao Kip"
+					},
+					{
+						"value": "LBP",
+						"label": "Lebanese Pound"
+					},
+					{
+						"value": "LKR",
+						"label": "Sri Lankan Rupee"
+					},
+					{
+						"value": "LRD",
+						"label": "Liberian Dollar"
+					},
+					{
+						"value": "LSL",
+						"label": "Lesotho Loti"
+					},
+					{
+						"value": "LTL",
+						"label": "Lithuanian Litas"
+					},
+					{
+						"value": "LVL",
+						"label": "Latvian Lats"
+					},
+					{
+						"value": "LYD",
+						"label": "Libyan Dinar"
+					},
+					{
+						"value": "MAD",
+						"label": "Moroccan Dirham"
+					},
+					{
+						"value": "MDL",
+						"label": "Moldovan Leu"
+					},
+					{
+						"value": "MGA",
+						"label": "Malagasy Ariary"
+					},
+					{
+						"value": "MKD",
+						"label": "Macedonian Denar"
+					},
+					{
+						"value": "MMK",
+						"label": "Myanmar Kyat"
+					},
+					{
+						"value": "MNT",
+						"label": "Mongolian T\u00f6gr\u00f6g"
+					},
+					{
+						"value": "MOP",
+						"label": "Macanese Pataca"
+					},
+					{
+						"value": "MRO",
+						"label": "Mauritanian Ouguiya"
+					},
+					{
+						"value": "MTL",
+						"label": "Maltese Lira"
+					},
+					{
+						"value": "MUR",
+						"label": "Mauritian Rupee"
+					},
+					{
+						"value": "MVR",
+						"label": "Maldivian Rufiyaa"
+					},
+					{
+						"value": "MWK",
+						"label": "Malawian Kwacha"
+					},
+					{
+						"value": "MXN",
+						"label": "Mexican Peso"
+					},
+					{
+						"value": "MYR",
+						"label": "Malaysian Ringgit"
+					},
+					{
+						"value": "MZN",
+						"label": "Mozambican Metical"
+					},
+					{
+						"value": "NAD",
+						"label": "Namibian Dollar"
+					},
+					{
+						"value": "NGN",
+						"label": "Nigerian Naira"
+					},
+					{
+						"value": "NIO",
+						"label": "Nicaraguan C\u00f3rdoba"
+					},
+					{
+						"value": "NOK",
+						"label": "Norwegian Krone"
+					},
+					{
+						"value": "NPR",
+						"label": "Nepalese Rupee"
+					},
+					{
+						"value": "NZD",
+						"label": "New Zealand Dollar"
+					},
+					{
+						"value": "OMR",
+						"label": "Omani Rial"
+					},
+					{
+						"value": "PAB",
+						"label": "Panamanian Balboa"
+					},
+					{
+						"value": "PEN",
+						"label": "Peruvian Sol"
+					},
+					{
+						"value": "PGK",
+						"label": "Papua New Guinean Kina"
+					},
+					{
+						"value": "PHP",
+						"label": "Philippine Peso"
+					},
+					{
+						"value": "PKR",
+						"label": "Pakistani Rupee"
+					},
+					{
+						"value": "PLN",
+						"label": "Polish Z\u0142oty"
+					},
+					{
+						"value": "PYG",
+						"label": "Paraguayan Guaran\u00ed"
+					},
+					{
+						"value": "QAR",
+						"label": "Qatari Riyal"
+					},
+					{
+						"value": "RON",
+						"label": "Romanian Leu"
+					},
+					{
+						"value": "RSD",
+						"label": "Serbian Dinar"
+					},
+					{
+						"value": "RUB",
+						"label": "Russian Ruble"
+					},
+					{
+						"value": "RWF",
+						"label": "Rwandan Franc"
+					},
+					{
+						"value": "SAR",
+						"label": "Saudi Riyal"
+					},
+					{
+						"value": "SBD",
+						"label": "Solomon Islands Dollar"
+					},
+					{
+						"value": "SCR",
+						"label": "Seychellois Rupee"
+					},
+					{
+						"value": "SEK",
+						"label": "Swedish Krona"
+					},
+					{
+						"value": "SGD",
+						"label": "Singapore Dollar"
+					},
+					{
+						"value": "SHP",
+						"label": "Saint Helenian Pound"
+					},
+					{
+						"value": "SLL",
+						"label": "Sierra Leonean Leone"
+					},
+					{
+						"value": "SOS",
+						"label": "Somali Shilling"
+					},
+					{
+						"value": "SRD",
+						"label": "Surilabelse Dollar"
+					},
+					{
+						"value": "SSP",
+						"label": "South Sudanese Pound"
+					},
+					{
+						"value": "STD",
+						"label": "S\u00e3o Tom\u00e9 and Pr\u00edncipe Dobra"
+					},
+					{
+						"value": "SVC",
+						"label": "Salvadoran Col\u00f3n"
+					},
+					{
+						"value": "SZL",
+						"label": "Swazi Lilangeni"
+					},
+					{
+						"value": "THB",
+						"label": "Thai Baht"
+					},
+					{
+						"value": "TJS",
+						"label": "Tajikistani Somoni"
+					},
+					{
+						"value": "TMT",
+						"label": "Turkmenistani Manat"
+					},
+					{
+						"value": "TND",
+						"label": "Tunisian Dinar"
+					},
+					{
+						"value": "TOP",
+						"label": "Tongan Pa\u02bbanga"
+					},
+					{
+						"value": "TRY",
+						"label": "Turkish Lira"
+					},
+					{
+						"value": "TTD",
+						"label": "Trinvaluead and Tobago Dollar"
+					},
+					{
+						"value": "TWD",
+						"label": "New Taiwan Dollar"
+					},
+					{
+						"value": "TZS",
+						"label": "Tanzanian Shilling"
+					},
+					{
+						"value": "UAH",
+						"label": "Ukrainian Hryvnia"
+					},
+					{
+						"value": "UGX",
+						"label": "Ugandan Shilling"
+					},
+					{
+						"value": "UYU",
+						"label": "Uruguayan Peso"
+					},
+					{
+						"value": "UZS",
+						"label": "Uzbekistan Som"
+					},
+					{
+						"value": "VEF",
+						"label": "Venezuelan Bol\u00edvar"
+					},
+					{
+						"value": "VND",
+						"label": "Vietlabelse \u0110\u1ed3ng"
+					},
+					{
+						"value": "VUV",
+						"label": "Vanuatu Vatu"
+					},
+					{
+						"value": "WST",
+						"label": "Samoan Tala"
+					},
+					{
+						"value": "XAF",
+						"label": "Central African Cfa Franc"
+					},
+					{
+						"value": "XAG",
+						"label": "Silver (Troy Ounce)"
+					},
+					{
+						"value": "XAU",
+						"label": "Gold (Troy Ounce)"
+					},
+					{
+						"value": "XCD",
+						"label": "East Caribbean Dollar"
+					},
+					{
+						"value": "XDR",
+						"label": "Special Drawing Rights"
+					},
+					{
+						"value": "XOF",
+						"label": "West African Cfa Franc"
+					},
+					{
+						"value": "XPD",
+						"label": "Palladium"
+					},
+					{
+						"value": "XPF",
+						"label": "Cfp Franc"
+					},
+					{
+						"value": "XPT",
+						"label": "Platinum"
+					},
+					{
+						"value": "YER",
+						"label": "Yemeni Rial"
+					},
+					{
+						"value": "ZAR",
+						"label": "South African Rand"
+					},
+					{
+						"value": "ZMK",
+						"label": "Zambian Kwacha"
+					},
+					{
+						"value": "ZMW",
+						"label": "Zambian Kwacha"
+					},
+					{
+						"value": "ZWL",
+						"label": "Zimbabwean Dollar"
+					}
+				]
+			}
+    ]
+  },
+  {
+    "type": "section",
+    "items": 
+		[
+      {
+        "type": "heading",
+        "defaultValue": "BTC Wallet"
+      },
+      {
+				"type": "input",
+				"appKey": "KEY_BTC_NAME_1",
+				"defaultValue": "BTC Wallet",
+				"label": "Wallet Name",
+				"attributes": {
+					"placeholder": "eg: MyBTCWallet",
+					"limit": 36,
+					"type": "text"
+			}
+			},
+			{
+				"type": "input",
+				"appKey": "KEY_BTC_ADDRESS_1",
+				"defaultValue": "",
+				"label": "Bitcoin Address",
+				"attributes": 
+				{
+					"placeholder": "eg: 12345...",
+					"limit": 34,
+					"type": "text"
+				}
+			}
+    ]
+  },
+	{
+    "type": "section",
+    "items": 
+		[
+      {
+        "type": "heading",
+        "defaultValue": "LTC Wallet"
+      },
+      {
+				"type": "input",
+				"appKey": "KEY_LTC_NAME_1",
+				"defaultValue": "LTC Wallet",
+				"label": "Wallet Name",
+				"attributes": {
+					"placeholder": "eg: MyLTCWallet",
+					"limit": 36,
+					"type": "text"
+			}
+			},
+			{
+				"type": "input",
+				"appKey": "KEY_LTC_ADDRESS_1",
+				"defaultValue": "",
+				"label": "Litecoin Address",
+				"attributes": 
+				{
+					"placeholder": "eg: L2345...",
+					"limit": 34,
+					"type": "text"
+				}
+			}
+    ]
+  },
+	{
+    "type": "section",
+    "items": 
+		[
+      {
+        "type": "heading",
+        "defaultValue": "Donate BTC or LTC"
+      },
+      { 
+				"type": "text", 
+				"defaultValue": "Do you like my app? You can buy me a coffee by sending me some BTC or LTC. Copy the address below<h6>BTC: 1D9HuDKKYY5RiMmwicV4xfuRNhVNwtmNJP</h6><h6>LTC: LVVFkds7vYEN3yVDpXVPeMUtMYKZ8pNYbu</h6>" 
+			}
+    ]
+  },
+	{
+    "type": "submit",
+    "defaultValue": "Save Settings"
+  }
+
+  ];
